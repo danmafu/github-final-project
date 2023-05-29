06dd28ca54565671262cf6284e3eb78f9d7cf47c
Daniel Mafusire <64380767+danmafu@users.noreply.github.com>
2023-05-29 18:21:35 +1000

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: Daniel Mafusire (At Large)
    # Additional Authors:
-   # <your GitHub username>
+   # danmafu
 
    # Input:
    # p, principal amount
